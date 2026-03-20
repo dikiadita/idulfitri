f9620fa219ffae215925c7fcc47e6c31196f49f0
dikiadita <aditadiki86@gmail.com>
2026-03-20 10:08:28 +0700

    Create css.js

---
+++ js/css.js
@@ -0,0 +1,16 @@
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'muha-dark': '#0a2540', /* Biru gelap untuk depth */
+                        'muha-blue': '#0060a9', /* Biru khas Muhammadiyah */
+                        'gold-400': '#fbbf24',
+                        'gold-500': '#f59e0b',
+                    },
+                    fontFamily: {
+                        'sans': ['Outfit', 'sans-serif'],
+                        'serif': ['Playfair Display', 'serif'],
+                    }
+                }
+            }
+        }
